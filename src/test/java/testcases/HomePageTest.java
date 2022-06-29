cca5793e2c0d225c8050032dffcde828d3e4acc8
AnandhiTech <49861835+anandhi-k@users.noreply.github.com>
2022-06-29 18:16:24 +0530

    Initialized using WebDriverManager

--- src/test/java/testcases/HomePageTest.java
+++ src/test/java/testcases/HomePageTest.java
@@ -11,18 +11,22 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class HomePageTest {
 	WebDriver driver;
   
   @BeforeTest
   public void beforeTest() {
-	  System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-	  driver = new ChromeDriver();
+// 	  System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+// 	  driver = new ChromeDriver();
+	  WebDriverManager.edgedriver().setup();
+		driver = new EdgeDriver();
 	  driver.get("http://localhost:9090");
 		driver.manage().window().maximize();
 		 driver.manage().timeouts().implicitlyWait(2000, TimeUnit.SECONDS);
